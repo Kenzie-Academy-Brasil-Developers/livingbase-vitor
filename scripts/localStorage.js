d5abba1633237ca6daa5aee95fb5f3017da0f7b2
Vitor Galdino <httpvittor@gmail.com>
2022-10-26 20:14:46 -0300

    refactor: included new methods

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,9 +1,13 @@
 export class SetLocalStorage {
     static activeFilter = (value) => localStorage.setItem("filter", JSON.stringify(value));
+
+    static idPost = (value) => localStorage.setItem("idPost", JSON.stringify(value));
 }
 
 export class GetLocalStorage {
     static activeFilter = () => JSON.parse(localStorage.getItem("filter"));
+
+    static idPost = () => JSON.parse(localStorage.getItem("idPost"));
 }
 
 export class Capture {
@@ -12,7 +16,7 @@ export class Capture {
 
         allButtons.forEach(button => {
             button.onclick = async (e) => {
-                JSON.stringify(localStorage.setItem("idPost", e.target.dataset.idpost));
+                SetLocalStorage.idPost(e.target.dataset.idpost);
                 location.replace("/pages/post/");
             }
         });
