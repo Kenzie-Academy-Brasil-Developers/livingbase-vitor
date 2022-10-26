58073db42a2896931a5c5e0cc5f59bc15f821172
Vitor Galdino <httpvittor@gmail.com>
2022-10-25 23:40:41 -0300

    feat: call categories class

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,4 +1,6 @@
 import { toViewButton } from "../../scripts/activeButtons.js";
+import { categories } from "../../scripts/categoriesBar.js";
 
+categories.checkUlLength();
 
 toViewButton.categoriesButton();
