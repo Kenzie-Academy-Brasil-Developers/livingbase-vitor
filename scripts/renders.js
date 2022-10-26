49914af60078f0ca0b2c4c20a60db2205303311b
Vitor Galdino <httpvittor@gmail.com>
2022-10-26 19:59:05 -0300

    feat: included classes methods showCategories and showPosts

--- scripts/renders.js
+++ scripts/renders.js
@@ -1,6 +1,42 @@
+import { ToViewButton } from "./activeButtons.js";
+import { GetLocalStorage } from "./localStorage.js";
+import { Api } from "./request.js"
+
 export class Render {
 
-    static showCategories = () => {
+    static showCategories = async () => {
+        const apiRequest = await Api.getPosts();
+        const ul = document.querySelector(".categories-content");
+
+        apiRequest.slice(0, -1).forEach(elem => {
+            ul.insertAdjacentHTML("beforeend",
+                `<li><button data-ctgButton class="grey-btn">${elem.category}</button></li>`);
+
+        });
+
+        ToViewButton.categoriesButton();
+    }
+
+    static showPosts = async () => {
+        const ul = document.querySelector(".post-wrapper");
+        let data = await Api.getPosts();
+
+        if (GetLocalStorage.activeFilter() && GetLocalStorage.activeFilter() !== "Todos") {
+            const filterName = GetLocalStorage.activeFilter();
+            const filtered = data.filter(elem => elem.category === filterName);
+            data = filtered;
+        }
         
+        ul.innerHTML = "";
+        data.forEach(elem => {
+            ul.insertAdjacentHTML("beforeend", `
+            <li class="post">
+                <img src="${elem.image}" alt="${elem.title.split(" ")[0]} image">
+                <h3>${elem.title}</h3>
+                <p>${elem.description}</p>
+                <span data-idPost="${elem.id}" class="show-content">Acessar conteúdo</span>
+            </li>
+            `);
+        });
     }
 }
