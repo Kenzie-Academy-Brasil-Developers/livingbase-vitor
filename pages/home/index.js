85091f41c9b7b9cb1bafe12c3d05711f110bc9d2
Vitor Galdino <httpvittor@gmail.com>
2022-10-26 19:56:19 -0300

    feat: call classes methods

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,9 @@
 import { ToViewButton } from "../../scripts/activeButtons.js";
 import { ScrollCategories } from "../../scripts/categoriesBar.js";
+import { Render } from "../../scripts/renders.js";
+
+Render.showCategories();
+Render.showPosts();
 
 ScrollCategories.checkUlLength();
 
