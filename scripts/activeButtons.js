4f95d70ffb3d199895f8f998feb04b702506db5d
Vitor Galdino <httpvittor@gmail.com>
2022-10-25 23:36:00 -0300

    feat: created class toViewButton

---
+++ scripts/activeButtons.js
@@ -0,0 +1,12 @@
+export  class toViewButton {
+    static categoriesButton = () => {
+        const allButtons = document.querySelectorAll("[data-ctgButton]")
+        
+        allButtons.forEach(button => {
+            button.onclick = () => {
+                allButtons.forEach(elem => elem.classList.remove("primary-btn"));
+                button.classList.add("primary-btn");
+            } 
+        });
+    }
+}
