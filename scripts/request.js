d8809db7fabba51fecf1f52165a9c2619c07430c
Vitor Galdino <httpvittor@gmail.com>
2022-10-26 19:59:55 -0300

    feat: included classes methods to make all request at api

--- scripts/request.js
+++ scripts/request.js
@@ -1,20 +1,27 @@
 export class Api {
     
-    static getPosts = () => {
-        fetch(`https://m2-api-living.herokuapp.com/news?page=20`, {
-            method: "GET",
-        })
-        .then(res => res.json())
-        .then(res => res.news)
-        .catch(err => console.log(err));
+    static getPosts = async () => {
+        try {
+            const request = await fetch(`https://m2-api-living.herokuapp.com/news?page=20`, {
+                method: "GET",
+            });
+
+            const response = await request.json();
+            return response.news;
+
+        } catch (err) {
+            return err;
+        }
     }
 
-    static getPostById = id => {
-        fetch(`https://m2-api-living.herokuapp.com/news/${id}`, {
-            method: "GET",
-        })
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err));
+    static getPostById = async (id) => {
+        try {
+            const request = await fetch(`https://m2-api-living.herokuapp.com/news/${id}`, {
+                method: "GET",
+            });
+            return request.json();
+        } catch (err) {
+            return err;
+        }
     }
 }
