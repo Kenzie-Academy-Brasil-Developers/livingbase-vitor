d5740548c46aeb146a49ec0af8420e9c1ce967b9
Vitor Galdino <httpvittor@gmail.com>
2022-10-27 18:33:19 -0300

    feat: included functional observer method

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,21 +1,20 @@
-let count = 0;
+import { Render } from "./renders.js";
+
+export let count = 1;
 
 export class Observer {
 
     static checkFinalPost = async (permission = true) => {
         if (!permission) {
+            count = 0;
             return false;
         }
         const intersectionObserver = new IntersectionObserver((entries) => {
             entries.some((entry) => {
-                if (entry.isIntersecting) {
-                    console.log("estamos observando!")
-                    count++
-                    // StorageToRender.apiPosts(count);
-                    // console.log(count)
-                } else {
-                    console.log("não estamos observando!")
-                }
+                if (entry.isIntersecting && count <= 3) {
+                    Render.showPosts(count);
+                    count++;
+                } 
             });
         });
 
