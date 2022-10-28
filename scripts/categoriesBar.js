b51a59d17cf8f022711b8f3c6925b3ebe04a37ab
Vitor Galdino <httpvittor@gmail.com>
2022-10-27 23:55:22 -0300

    fix: duplicate render

--- scripts/categoriesBar.js
+++ scripts/categoriesBar.js
@@ -4,8 +4,8 @@ export class ScrollCategories {
 
         const nav = document.querySelector(".categories");
         const ul = document.querySelector(".categories-content");
-        if (ul.children.length >= 8) {
-            nav.insertAdjacentHTML("beforeend", `<button onclick="scrollToRight()" class="pass-btn right-pass"></button>`);
+        if (ul.children.length >= 8 && !document.querySelector(".pass-btn")) {
+            nav.insertAdjacentHTML("beforeend", `<button class="pass-btn right-pass"></button>`);
             this.scrollToRight();
         }
     }
@@ -19,7 +19,7 @@ export class ScrollCategories {
             ul.scrollBy(150, 0);
 
             if (!document.querySelector(".left-pass")) {
-                nav.insertAdjacentHTML("beforeend", `<button onclick="scrollToLeft()" class="pass-btn left-pass"></button>`);
+                nav.insertAdjacentHTML("beforeend", `<button class="pass-btn left-pass"></button>`);
                 this.scrollToLeft();
             }
         }
