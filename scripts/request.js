439b18053332baa2abc1c2cd6988e68823a338e1
Vitor Galdino <httpvittor@gmail.com>
2022-10-28 00:35:33 -0300

    refactor: little adjust on getPosts

--- scripts/request.js
+++ scripts/request.js
@@ -5,9 +5,8 @@ export class Api {
             const request = await fetch(`https://m2-api-living.herokuapp.com/news?page=${num}`, {
                 method: "GET",
             });
-            
+
             const response = await request.json();
-            console.log(response.news)
             return response.news;
 
         } catch (err) {
