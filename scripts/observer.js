0422838b3dd244e942af145c46a12a3d96d82595
Vitor Galdino <httpvittor@gmail.com>
2022-10-28 00:39:56 -0300

    feat: completed class method and all functional

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,26 +1,22 @@
 import { StorageToRender } from "./storage.js";
 
-let count = 0;
+export let count = 1;
 
 export class Observer {
 
     static checkFinalPost = async (permission = true) => {
         if (!permission) {
+            count = 1;
             return false;
         }
         const intersectionObserver = new IntersectionObserver((entries) => {
             entries.some((entry) => {
-                if (entry.isIntersecting) {
-                    console.log("estamos observando!")
-                    count++
-                    // StorageToRender.apiPosts(count);
-                    // console.log(count)
-                } else {
-                    console.log("não estamos observando!")
-                }
+                if (entry.isIntersecting && count <= 3) {
+                    StorageToRender.apiPosts(count);
+                    count++;
+                } 
             });
         });
-
         intersectionObserver.observe(document.querySelector(".post-wrapper").lastElementChild);
     }
 }
