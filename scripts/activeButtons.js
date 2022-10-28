4a6b3b99114f3261062feb2ae91d90e76f6af3dc
Vitor Galdino <httpvittor@gmail.com>
2022-10-28 00:38:50 -0300

    refactor: correction and adjust some points

--- scripts/activeButtons.js
+++ scripts/activeButtons.js
@@ -1,11 +1,12 @@
 import { GetLocalStorage, SetLocalStorage } from "./localStorage.js";
 import { Render } from "./renders.js";
+import { logStorage } from "./storage.js";
 
 export class ToViewButton {
     static categoriesButton = (bool) => {
         const allButtons = document.querySelectorAll("[data-ctgButton]")
         const filteredName = GetLocalStorage.activeFilter();
-        
+
         allButtons.forEach(button => {
             if (filteredName && bool) {
                 if (button.innerHTML === filteredName) {
@@ -16,14 +17,12 @@ export class ToViewButton {
 
             button.onclick = () => {
                 allButtons.forEach(elem => elem.classList.remove("primary-btn"));
+                if (!bool) setTimeout(() => location.replace("/pages/home/"), 400);
                 
-                Render.showPosts();
                 SetLocalStorage.activeFilter(button.innerHTML);
                 button.classList.add("primary-btn");
+                Render.showPosts(logStorage);
 
-                if (!bool) {
-                    setTimeout(() => location.replace("/pages/home/"), 400);
-                }
             }
         });
     }
